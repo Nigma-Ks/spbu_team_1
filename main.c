7b8b886f2d7aff7602f1630dc10d4a1de9e4f5f2
Nigma-Ks <114099264+Nigma-Ks@users.noreply.github.com>
2022-10-12 22:25:35 +0500

    Add list_create and list_free functions

--- main.c
+++ main.c
@@ -12,3 +12,65 @@ typedef struct List {
     struct ListElement *firstElement;
 } List;
 
+int create(List **newList) {
+    *newList = (List *) calloc(1, sizeof(List));
+    if (newList == NULL) {
+        return -2;
+    }
+    return 0;
+}
+
+void freeList(List *list) {
+    if (list == NULL) {
+        return;
+    }
+    ListElement *currElement = list->firstElement;
+    ListElement *nextCurrElement = list->firstElement;
+    while (currElement != NULL) {
+        nextCurrElement = currElement->nextElement;
+        free(currElement);
+        currElement = nextCurrElement;
+    }
+    list->firstElement = NULL;
+}
+
+bool testCreate() {
+    List *list = NULL;
+    int error = create(&list);
+    if (list != NULL) {
+        free(list);
+        return true;
+    }
+    free(list);
+    return false;
+}
+
+
+int testFree() {
+    List *list = NULL;
+    int error = create(&list);
+    if (error == -2) {
+        return -2;
+    }
+    list->firstElement = malloc(sizeof(ListElement));
+    list->firstElement->value = 1;
+    ListElement *secondElement = malloc(sizeof(ListElement));
+    secondElement->value = 2;
+    list->firstElement->nextElement = secondElement;
+    secondElement->nextElement = NULL;
+    freeList(list);
+    if (list->firstElement == NULL) {
+        free(list);
+        return 0;
+    }
+    free(list);
+    return -1;
+}
+
+int main() {
+    if (testCreate() && !testFree()) {
+        printf("Successful creating and freeing the list\n");
+    } else {//-return for merge team's code
+        printf("Unsuccessful creating and freeing the list\n");
+    }
+}
