c84052114f8b8cbd52b606e8004bf452900640b0
tanyaponomaryova <tat.ponom2004@gmail.com>
2022-10-12 18:02:16 +0300

    add ListLength

--- main.c
+++ main.c
@@ -12,3 +12,20 @@ typedef struct List {
     struct ListElement *firstElement;
 } List;
 
+
+int ListLength( List *list, size_t *len)
+{
+    if (list == NULL || len == NULL)
+    {
+        return -1;
+    }
+    *len = 0;
+    ListElement *currentElement = list->firstElement;
+    while (currentElement != NULL)
+    {
+        ++(*len);
+        currentElement = currentElement->nextElement;
+    }
+    return 0;
+}
+
